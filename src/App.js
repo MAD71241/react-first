3d32cfe016a738f6002fd9805a1858c1e7cf8088
MAD71241 <michele.deliso@gmail.com>
2021-07-27 22:46:25 +0200

    installato npm watch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a first try at React
         </a>
       </header>
     </div>
