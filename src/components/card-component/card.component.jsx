b291be2c6076910ee0d533f5d1e87e00ce4635e6
MAD71241 <michele.deliso@gmail.com>
2021-08-04 21:23:18 +0200

    stile ok in card-container

--- src/components/card-component/card.component.jsx
+++ src/components/card-component/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card.style.css';
 
 export const CardComponent = (props) => {
-    return <div>
+    return <div className="card-container">
         <h1>{props.monster.name}</h1>
     </div>
 }
