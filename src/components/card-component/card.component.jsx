ca4845537db8b56ed8a8e8423fccf66b7cc5d226
MAD71241 <michele.deliso@gmail.com>
2021-08-04 21:35:01 +0200

    aggiunta generazione dinamica immagini

--- src/components/card-component/card.component.jsx
+++ src/components/card-component/card.component.jsx
@@ -3,6 +3,7 @@ import './card.style.css';
 
 export const CardComponent = (props) => {
     return <div className="card-container">
-        <h1>{props.monster.name}</h1>
+        <img src={`https://robohash.org/${props.monster.name}.png?set=set4&size=150x150`} alt="cute kitten" />
+        <h2>{props.monster.name}</h2>
     </div>
 }
